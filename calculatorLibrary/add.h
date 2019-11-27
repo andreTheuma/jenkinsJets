2b848e187f197f2d4f270cbeddbc27ba9e1e4e05
Andre Theuma <andre.theuma.19@um.edu.mt>
2019-11-27 11:16:13 +0100

    Main+add

---
+++ calculatorLibrary/add.h
@@ -0,0 +1,4 @@
+int add(add1, addd2)
+{
+    return add1+add2;
+}
--- calculatorLibrary/library.c
+++ calculatorLibrary/library.c
@@ -1,7 +1,35 @@
 #include "library.h"
-
+#include "add.h"
 #include <stdio.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+void main()
+{
+    int option;
+    int status;
+    int add1,add2;
+
+    printf("Please enter an option\n");
+   status = scanf("%d" , option);
+
+    do
+    {
+        switch(option)
+        {
+            case 1:
+                printf("Adding\n");
+                printf("Please enter two numbers to add together");
+                scanf("%f" , add1);
+                scanf("%f" , add2);
+                add(add1,add2);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
+    }
+        while (status == 1);
 }
