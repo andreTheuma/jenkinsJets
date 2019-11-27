679fc343b25ce450d0763fd9f8832b5d706db203
Luke Dalli <luke.dalli.19@um.edu.mt>
2019-11-27 11:49:16 +0100

    Debugging

--- calculatorLibrary/library.c
+++ calculatorLibrary/library.c
@@ -8,13 +8,10 @@ void main()
 {
     int option;
     int status;
-<<<<<<< HEAD
+
     float num1, num2;
-=======
+
     float add1,add2,mult1,mult2;
-
-
->>>>>>> 099e98fc27b97302ba54c5d73c06efadc06b6d53
 
     printf("Please enter an option\n");
     status = scanf("%d" , &option);
@@ -25,31 +22,26 @@ void main()
         {
             case 1:
                 printf("Adding\n");
-<<<<<<< HEAD
+
                 printf("Please enter two numbers to add together: ");
-                scanf("%f" , num1);
-                scanf("%f" , num2);
+                scanf("%f" , &num1);
+                scanf("%f" , &num2);
                 printf("The addition of %.2f and %.2f is %.2f",num1,num2,add(num1,num2));
-=======
-                printf("Please enter two numbers to add together");
-                scanf("%f" , &add1);
-                scanf("%f" , &add2);
-                printf("The addition of %.2f and %.2f is %.2f",add1,add2,add(add1,add2));
->>>>>>> 099e98fc27b97302ba54c5d73c06efadc06b6d53
+
                 break;
             case 2:
                 printf("Multiplying\n");
                 printf("Please enter two numbers to multiply by each other");
                 scanf("%f" , &mult1);
                 scanf("%f", &mult2);
-                printf("The multiplication of %.2f and %.2f is %.2f",mult1,mult2,multiply(mult1,mult2));
+                printf("The multiplication of %.2f and %.2f is %.2f",num1,num2,multiply(num1,num2));
                 break;
             case 3:
                 printf("Subtracting\n");
                 printf("Please enter 2 numbers to subtract the second from the first: ");
                 scanf("%f" , &num1);
                 scanf("%f" , &num2);
-                printf("The result of %.2f minus %.2f is %.2f",num1,num2,add(num2,num1));
+                printf("The result of %.2f minus %.2f is %.2f",num1,num2,add(num1,num2));
                 break;
             case 4:
                 break;
