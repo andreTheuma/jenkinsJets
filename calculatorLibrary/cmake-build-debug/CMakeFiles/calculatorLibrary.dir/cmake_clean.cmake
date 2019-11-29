0c828607703ca61e0a7b16eba51c259c904d02ac
Andre Theuma <andre.theuma.19@um.edu.mt>
2019-11-29 11:46:17 +0100

    adding file

---
+++ calculatorLibrary/cmake-build-debug/CMakeFiles/calculatorLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculatorLibrary.dir/library.c.o"
+  "libcalculatorLibrary.a"
+  "libcalculatorLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculatorLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
